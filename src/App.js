260cab1bbe3fb166c719b39df93b99cf1adec834
DESKTOP-B8HD5AP\PCZone.ge <ichkh2017@agruni.edu.ge>
2022-03-02 23:01:13 +0400

    creating welcome page component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import WelcomePage from './WelcomePage';
+
+function App() {
+
+  return(
+    <WelcomePage/>
+  )
+}
+
+export default App;
---
+++ src/WelcomePage.js
@@ -0,0 +1,25 @@
+import React,{Fragment} from 'react';
+import stars from './img/stars.png';
+import rocketman from './img/rocketman.png';
+import './WelcomePage.css';
+
+function WelcomePage() {
+  const Click=()=>{
+    console.log("clicked");
+  }
+  return (
+    <Fragment>
+        <img className='stars_image' src={stars} alt="stars"/>
+        <div className='content'>
+            <h1 className='main_title'>welcome rocketeer!</h1>
+            <button onClick={Click} className='start_button'>start questionnaire</button>
+            <h3 className='submitted_app_button'>submitted applications</h3>
+            <img className='rocketman' src={rocketman} alt="rocketman"/>
+            
+        </div>
+        
+    </Fragment>
+  )
+}
+
+export default WelcomePage;
